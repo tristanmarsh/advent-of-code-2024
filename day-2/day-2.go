364f327ce64644f2f84180ed793fc8e523977e9e
Tristan Marsh <tristanmarsh@live.com>
2024-12-03 20:55:50 -0600

    day 2 part 2

--- day-2/day-2.go
+++ day-2/day-2.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -21,23 +20,28 @@ func main() {
 		part1()
 	case 2:
 		part2()
-	default:
-		part1()
 	}
 }
 
-// given a list of n integers across y lines
-// - determine the number of valid lines where a line
-// - contains numbers all increasing or all decreasing
-// - increases or decreases only by 1, 2, or 3
+// Given a list of n levels (integers) across y reports (lines)
+// - determine the number of valid reports where a line
+// - contains reports (numbers) all
+// - increasing or decreasing by 1, 2, or 3
 func part1() {
 	fmt.Println("Part 1")
 	input := loadInputFile()
 	valid := countValidLines(input)
 	fmt.Println(valid)
 }
+
+// Given the same list of n levels (integers) across y reports (lines)
+// - revealuate with the same criteria if each report is valid
+// - but allowing for a single level (integer) to be pruned
 func part2() {
 	fmt.Println("Part 2")
+	input := loadInputFile()
+	valid := countValidLinesPart2(input)
+	fmt.Println(valid)
 }
 
 func loadInputFile() string {
@@ -50,25 +54,47 @@ func loadInputFile() string {
 
 func countValidLines(input string) (valid int) {
 	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if isValid(line) {
+	for _, stringLine := range lines {
+		line := lineToIntSlice(stringLine)
+		if isLineValid(line) {
 			valid++
 		}
 	}
 	return
 }
 
+func countValidLinesPart2(input string) (valid int) {
+	lines := strings.Split(input, "\n")
+	for _, stringLine := range lines {
+		line := lineToIntSlice(stringLine)
+		if isLineValid(line) || hasValidSubset(line) {
+			valid++
+		}
+	}
+	return
+}
+
+func hasValidSubset(line []int) bool {
+	for i := 0; i < len(line); i++ {
+		subset := copySliceWithoutIndex(line, i)
+		isSubsetValid := isLineValid(subset)
+		if isSubsetValid {
+			return true
+		}
+	}
+	return false
+}
+
+type Direction int
+
 const (
-	Ascending = iota
-	Descending
+	Ascending  Direction = 0
+	Descending Direction = 1
 )
 
-func isValid(lineString string) bool {
+func isLineValid(line []int) bool {
 	direction := Ascending
-	line := lineToIntSlice(lineString)
-	firstNumber := line[0]
-	lastNumber := line[len(line)-1]
-	if firstNumber > lastNumber {
+	if line[0] > line[len(line)-1] {
 		direction = Descending
 	}
 
@@ -76,18 +102,32 @@ func isValid(lineString string) bool {
 		// skip first iteration
 		if i > 0 {
 			previous := line[i-1]
-			diff := math.Abs(float64(x) - float64(previous))
-			if diff > 3 || diff == 0 {
-				return false
-			}
-			if direction == Ascending && x < previous {
-				return false
-			}
-			if direction == Descending && x > previous {
+			isPairValid := isPairValid(x, previous, direction)
+			if !isPairValid {
 				return false
 			}
 		}
 	}
+	return true
+}
+
+func isPairValid(x int, prev int, direction Direction) bool {
+	diff := x - prev
+	if diff < 0 {
+		diff = -diff
+	}
+	if diff > 3 || diff == 0 {
+		return false
+	}
+
+	if direction == Ascending && x < prev {
+		return false
+	}
+
+	if direction == Descending && x > prev {
+		return false
+	}
+
 	return true
 }
 
@@ -102,4 +142,14 @@ func lineToIntSlice(lineString string) (line []int) {
 		line = append(line, i)
 	}
 	return
+}
+
+// The source of all my pain
+// copy on write does not prevent the underlying slice entires from being modified
+// an explicit new slice must be created with make and copy to prevent modification of the passed slice
+func copySliceWithoutIndex(s []int, index int) []int {
+	// Create a new slice to ensure the original is unmodified
+	newSlice := make([]int, len(s))
+	copy(newSlice, s)
+	return append(newSlice[:index], newSlice[index+1:]...)
 }
